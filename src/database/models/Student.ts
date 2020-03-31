102850f56bc7acc5137a2735554273ba72168d81
sauerbraten <willing.alexander@googlemail.com>
2020-03-31 20:12:04 +0200

    add most columns to student DB model

--- src/database/models/Student.ts
+++ src/database/models/Student.ts
@@ -1,11 +1,20 @@
 import { Model, Table, Column } from "sequelize-typescript";
 
 @Table({
-	timestamps: false
+  timestamps: false,
 })
 export class Student extends Model<Student> {
-	@Column firstname: string;
-	@Column lastname: string;
-	@Column({ primaryKey: true }) email: string;
-	@Column verified: boolean;
+  @Column firstname: string;
+  @Column lastname: string;
+  @Column({ primaryKey: true }) email: string;
+  @Column verified: boolean;
+  @Column subjects: string;
+  @Column phone: string;
+  @Column birthday: Date;
+  @Column msg: string;
+  @Column screener: string;
+  @Column invited: boolean;
+  @Column feedback: string;
+  @Column({ field: "comment_screener" }) commentScreener: string;
+  @Column({ field: "knowscsfrom" }) knowsCoronaSchoolFrom: string;
 }
