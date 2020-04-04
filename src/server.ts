b90ea3f6ba1aa4b91f5be6e812dd81cab0d207cc
Leon Erath <leon-erath@hotmail.de>
2020-04-04 17:21:34 +0200

    test

--- src/server.ts
+++ src/server.ts
@@ -66,6 +66,8 @@ app.use(
       ...sessionConfig,
       rolling: true,
       renew: true,
+      secure: true,
+      sameSite: "none",
       store: redisStore({
         // eslint-disable-next-line @typescript-eslint/camelcase
         enable_offline_queue: false,
