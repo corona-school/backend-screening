03a7a244845446266761517d96825c6165a7c65c
Jonas Wilms <JonasWilms2000@gmail.com>
2020-12-23 20:00:02 +0100

    Support instructor pagination

--- src/services/backendApiService.ts
+++ src/services/backendApiService.ts
@@ -209,11 +209,12 @@ export const apiService = {
 
   async getInstructors(
     screeningStatus: ScreeningStatus.Accepted | ScreeningStatus.Rejected | ScreeningStatus.Unscreened,
-    search: string
+    search?: string,
+    page?: string
   ): Promise<Array<Student & { __screening__: Screening }>> {
     try {
       const { status, data } = await axios.get(`${API}instructors`, {
-        params: { screeningStatus, search },
+        params: { screeningStatus, search, page },
       });
       if (status !== 200)
         throw "Retrieving courses responded with non 200 return code " + status;
