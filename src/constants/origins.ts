c97f7db31270aaac179fbfcf164d477823589df8
Lennard Schiefelbein <lennard@familie-schiefelbein.de>
2021-05-15 13:19:33 +0200

    Allow lern-fair.de origins

--- src/constants/origins.ts
+++ src/constants/origins.ts
@@ -1,5 +1,6 @@
 export const validOrigins = [
   "http://localhost:3000",
+  "http://localhost:3001",
   "http://localhost:3002",
   "https://corona-school-admin-dev.herokuapp.com",
   "https://corona-school-admin.herokuapp.com",
@@ -7,8 +8,10 @@ export const validOrigins = [
   "https://corona-student-app.herokuapp.com",
   "https://authentication.corona-school.de",
   "https://screeners.corona-school.de",
+  "https://authentication.lern-fair.de",
+  "https://screeners.lern-fair.de",
 ];
 
 export const validOriginRegexes = [
-  /^https:\/\/(cs-screening-app|cs-screening-admin)-(pr-[0-9]+|br-[-a-z0-9]+).herokuapp.com$/
-]
+  /^https:\/\/(cs-screening-app|cs-screening-admin)-(pr-[0-9]+|br-[-a-z0-9]+).herokuapp.com$/,
+];
